2361813a69dfdb8427edac96eaabd275977fedec
karendaa <karendaa@stud.ntnu.no>
2020-09-18 11:41:33 +0200

    added correct music

--- p2/src/components/Audio.tsx
+++ p2/src/components/Audio.tsx
@@ -1,28 +1,18 @@
 import React, { useState } from "react";
 
 
-interface Props {
-  count: number;
-}
 
-
-export default function Audio(props: Props) {
+export default function Audio() {
   const [error, setError] = useState(null);
-  const audioList: any = [
-    require('../assets/media/bensound-ukulele.mp3'),
-    require('../assets/media/bensound-buddy.mp3'),
-    require('../assets/media/bensound-creativeminds.mp3'),
-    require('../assets/media/bensound-jazzyfrenchy.mp3'),
-    require('../assets/media/bensound-littleidea.mp3')
-  ];
+  const audio = require('../assets/media/bensound-buddy.mp3');
 
   if (error) {
     return <div>Error: </div>;
   } else {
     return (
       <div className="audioWrapper">
-        <audio controls autoPlay>
-          <source src={audioList[props.count]}  type='audio/mpeg'/>
+        <audio controls >
+          <source src={audio}  type='audio/mpeg'/>
         </audio>
       </div>
     );
